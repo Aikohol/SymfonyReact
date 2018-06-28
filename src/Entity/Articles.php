3dcfaab31eba9f01963c71b7fcf58e2f4f1bc025
Tio Gobin <tio.gobin@epitech.eu>
2018-06-28 03:24:21 +0200

    Ajout des dependences form orm doctrine maker twig validator

--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -20,4 +20,52 @@ class Articles
     {
         return $this->id;
     }
+
+	/**
+	*@ORM\Column(type="string")
+	**/
+	private $name;
+
+	/**
+	*@return mixed
+	**/
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	*@param mixed $name
+	**/
+
+	public function setName($name) : void
+	{
+		$this->name = $name;
+	}
+
+	/**
+	*@ORM\Column(type="text")
+	**/
+
+	private $description;
+
+	/**
+	*@return mixed
+	**/
+
+	public function getDescription()
+	{
+		return $this->description;
+	}
+
+	/**
+	*@param mixed $description
+	**/
+
+	public function setDescription($description) : void
+	{
+		$this->description = $description;
+	}
+
 }
