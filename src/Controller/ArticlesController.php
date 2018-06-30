281229e684ed0e5355ab81a27848e4f8be3b3bc0
Tio Gobin <tio.gobin@epitech.eu>
2018-06-30 10:16:40 +0200

    Mise en place de l'API REST avec l'entity Articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,25 +4,34 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 // use Symfony\Component\Routing\Annotation\Annotation;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use JMS\Serializer\SerializationContext;
 use App\Entity\Articles;
 use App\Repository\ArticlesRepository;
 
 class ArticlesController extends Controller
 {
-    /**
-     * @Route("/articles", name="articles")
-     */
-    public function index(ArticlesRepository $repo)
-    {
-		// $repo = $this->getDoctrine()->getRepository(Articles::class);
+	/**
+	* @Route("/articles", name="articles")
+	*/
+	public function index(ArticlesRepository $repo)
+	{
 		$articles = $repo->findAll();
+		$data = $this->get('jms_serializer')->serialize($articles, 'json',
+		SerializationContext::create()->setGroups(['list']));
 
-        return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+
+		return $response;
+		return $this->render('articles/index.html.twig', [
+			'controller_name' => 'ArticlesController',
 			'articles' => $articles,
-        ]);
-    }
+		]);
+	}
 
 	/**
 	* @Route("/", name="home")
@@ -36,11 +45,38 @@ class ArticlesController extends Controller
 	**/
 
 	public function show(Articles $article) {
-		// $repo = $this->getDoctrine()->getRepository(Articles::class);
-		// $article = $repo->find($id);
+		$data = $this->get('jms_serializer')->serialize($article, 'json',
+		SerializationContext::create()->setGroups(['detail']));
 
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+
+		return $response;
 		return $this->render('articles/show.html.twig', [
 			'article' => $article,
 		]);
+	}
+
+	/**
+	* @Route("/articles_create")
+	* @Method("POST")
+	**/
+	public function new(Request $request) {
+		$article = new Articles();
+
+		$data = $request->getContent();
+		$data = json_decode($data);
+
+		$manager = $this->getDoctrine()->getManager();
+
+		foreach($data as $key => $value) {
+			$set = 'set' . ucfirst($key);
+			$article->{$set}($value);
+		}
+		$manager->persist($article);
+
+		$manager->flush();
+
+		return new Response('', Response::HTTP_CREATED);
 	}
 }
--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -3,38 +3,50 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
 
 /**
 * @ORM\Entity(repositoryClass="App\Repository\ArticlesRepository")
+* @ORM\Entity
+* @ORM\Table()
 */
 class Articles
 {
+	public function __construct() {
+		$this->createdAt = new \DateTime();
+	}
 	/**
 	* @ORM\Id()
 	* @ORM\GeneratedValue()
 	* @ORM\Column(type="integer")
+	* @Serializer\Groups({"list"})
 	*/
 	private $id;
 
 	/**
 	* @ORM\Column(type="string", length=255)
+	* @Serializer\Groups({"detail", "list"})
 	*/
 	private $name;
 
 	/**
 	* @ORM\Column(type="text")
+	* @Serializer\Groups({"detail"})
 	*/
 	private $content;
 
 	/**
 	* @ORM\Column(type="string", length=255)
+	* @Serializer\Groups({"detail", "list"})
 	*/
 	private $image;
 
 	/**
 	* @ORM\Column(type="datetime")
+	* @Serializer\Groups({"detail", "list"})
 	*/
 	private $createdAt;
+
 
 	public function getId()
 	{
